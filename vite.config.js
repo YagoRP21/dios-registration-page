ed51802bd6e0b94c41e08c3656f237c96c9cf09f
YagoRP <yagoribeiropereira21@gmail.com>
2022-10-08 23:49:26 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pagina-cadastro-dio/',
-  build: {
-    emptyOutputDir: false
-  },
+  base: '/dios-registration-page/',
   plugins: [react()]
 })
