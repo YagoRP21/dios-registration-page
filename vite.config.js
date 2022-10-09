dc763473a0ed0b4765ddde70a6e4fce814599dd4
YagoRP <yagoribeiropereira21@gmail.com>
2022-10-08 23:17:37 -0300

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/pagina-cadastro-dio/',
+  build: {
+    emptyOutputDir: false
+  },
   plugins: [react()]
 })
